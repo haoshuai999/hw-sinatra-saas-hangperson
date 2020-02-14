acc81fdcde6bf662927fae302f4fa16abb8a4551
haoshuai999 <626593718@qq.com>
2020-02-14 13:34:39 -0500

    add task2 of hw2

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,61 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+
   def initialize(word)
     @word = word
+    @guesses = ""
+    @wrong_guesses = ""
+  end
+
+  def guess(letter)
+    if letter =~ /[[:alpha:]]/
+      letter.downcase!
+      if @word.include?(letter) && !@guesses.include?(letter)
+        @guesses += letter
+        return true
+      elsif !@word.include?(letter) && !@wrong_guesses.include?(letter)
+        @wrong_guesses += letter
+        return true
+      else
+        return false
+      end
+    else
+      raise ArgumentError
+    end
+  end
+
+  def word_with_guesses
+    displayed_word = ""
+    @word.each_char do |letter|
+      if @guesses.include?(letter)
+        displayed_word += letter
+      else
+        displayed_word += "-"
+      end
+    end
+    puts displayed_word
+    return displayed_word
+  end
+
+  def check_win_or_lose
+    i = 0
+    if @wrong_guesses.length == 7
+      return :lose
+    end
+    @word.each_char do |letter|
+      if @guesses.include?(letter)
+        i += 1
+      end
+    end
+    if i == @word.length
+      return :win
+    else
+      return :play
+    end
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
